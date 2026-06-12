658656d379a5032a290b59790a5f6e3da605e19c
divyanshsonta123-creator <divyanshsonta123@gmail.com>
2026-06-12 22:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (96.39%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        ListNode curr = head;
+
+        while(curr != null){
+
+            while(curr.next != null && curr.val == curr.next.val){
+                curr = curr.next;
+            }
+
+            if(prev.next == curr){
+                prev = prev.next; // unique node
+            }
+            else{
+                prev.next = curr.next; // remove duplicate block
+            }
+
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
